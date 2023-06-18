9d920dbf2939f9cbf22bf99915a8413447b3b3b7
Sandra G <sandyjtech@gmail.com>
2023-06-18 08:56:44 -0700

    created post request

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+
+  
+// method: "POST" is missing from the object below
+function submitData(name, email) {
+    
+    const formData = {
+        name: name,
+        email: email,
+      };
+
+const submitData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+ return fetch("http://localhost:3000/users", submitData)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+      const id = object.id;
+      document.body.innerHTML = `User ID: ${id}`;
+    })
+    .catch(function (error) {
+      alert("Did not get user data!");
+      document.body.innerHTML = `Ohh No!: ${error.message}`;
+    });
+}
+submitData("Steve", "steve@steve.com");  
